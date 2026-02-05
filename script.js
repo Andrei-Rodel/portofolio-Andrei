9f5cef0b048b62957468d7f1ad05022624c77eaf
Andrei Rodel <andrei_rodel@hotmail.com>
2026-02-05 10:04:28 -0300

    alterações de projeto

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const nav = document.querySelector(".nav");
 menuBtn.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+// ANIMAÇÃO REVEAL AO SCROLL
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+  reveals.forEach((el) => {
+    const top = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if(top < windowHeight - 80){
+      el.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
