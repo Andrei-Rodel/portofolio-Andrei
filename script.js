55fb0f6ec8de15e45f1242aec55ca317f5fd0257
Andrei Rodel <andrei_rodel@hotmail.com>
2026-02-06 11:13:26 -0300

    add area de certificados

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 const menuBtn = document.getElementById("menuBtn");
 const nav = document.querySelector(".nav");
 
-menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
+menuBtn.onclick = () => nav.classList.toggle("active");
 
-// ANIMAÇÃO REVEAL AO SCROLL
 const reveals = document.querySelectorAll(".reveal");
-
-function revealOnScroll(){
-  reveals.forEach((el) => {
-    const top = el.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    if(top < windowHeight - 80){
+function reveal(){
+  reveals.forEach(el=>{
+    if(el.getBoundingClientRect().top < window.innerHeight - 80){
       el.classList.add("show");
     }
   });
 }
-
-window.addEventListener("scroll", revealOnScroll);
-revealOnScroll();
+window.addEventListener("scroll", reveal);
+reveal();
